c86480511218faec2af1b5e2663b65a84d650cbe
trousyt <troy@snappengr.com>
2013-06-15 17:51:54 -0700

    implemented board

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-io.connection('connection', function(socket) {
+// io.connection('connection', function(socket) {
 	
-});
+// });
