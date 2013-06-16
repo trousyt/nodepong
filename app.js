572edf2e9360b58985fd897d6a20166a917a3098
moerion <moerion90@gmail.com>
2013-06-15 17:49:23 -0700

    changed app.js

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ var express = require('express')
 
 var app = express();
 
+var socket1, socket2;
+var paddles = [0, 0];
+var width = 640, height = 480;
+var ball_speed = 20;
+var ball_angle = 0;
+var ball_pos = {x: width/2, y: height/2};
+
+var paddle_width = 20;
+var paddle_height = 80;
+var ball_size = 20;
+
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
@@ -38,6 +50,37 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-io.connection('connection', function(socket) {
-	
+io.on('connection', function(socket) {
+	if (!socket1){
+		socket1 = socket;	
+	}else if (!socket2){
+		socket2 = socket;
+	}
+
+	var i;
+
+	socket.emit('init', function(){
+		if (socket == socket1){
+			i = 1;
+		}else if (socket == socket2){
+			i = 2;
+		}
+
+		return {player: i, paddle_width: 20, paddle_height: 80, ball_size: 20};
+	});
+
+	socket.on('update', function(data){
+		if (data.player == 1){
+			paddles[0] = data.y;
+		}else if (data.player == 2){
+			paddles[1] = data.y;
+		}
+	});
+
+	setInterval(function(){
+		ball_pos.x = ball_pos.x + Math.cos(ball_angle) * ball_speed;
+		ball_pos.y = ball_pos.y - Math.sin(ball_angle) * ball_speed;		
+		socket.emit('draw', ball_pos);
+	}, 100);
+
 });
